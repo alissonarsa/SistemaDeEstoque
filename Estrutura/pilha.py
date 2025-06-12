1bcc44c5b8e38bfe9604a0a9062e6852c6d76f06
alissonarsa <alissonarsa@gmail.com>
2025-06-11 22:09:52 -0300

    classe de pilha ok

---
+++ Estrutura/pilha.py
@@ -0,0 +1,51 @@
+from typing import Any, List # list de qualquer tipo
+
+class Pilha:
+    
+    def __init__(self, capacidade: int = 5): # pilha iniciada com a capacidade maxima
+        
+        self.capacidade = capacidade
+        self.itens: List[Any] = []  # A pilha será armazenada em uma lista Python.
+
+    def esta_vazia(self) -> bool:
+        return len(self.itens) == 0
+
+    def esta_cheia(self) -> bool:
+        return len(self.itens) == self.capacidade
+
+    def empilhar(self, item: Any) -> bool:
+        if not self.esta_cheia():
+            self.itens.append(item)
+            return True
+        print(f"ERRO: A pilha está cheia! Não é possível empilhar o item {item}.")
+        return False
+
+    def desempilhar(self) -> Any:
+        if not self.esta_vazia():
+            return self.itens.pop() # remove o ultimo/topo!
+        print("ERRO: A pilha está vazia! Não é possível desempilhar.")
+        return None
+
+    def ver_topo(self) -> Any:
+        if not self.esta_vazia():
+            return self.itens[-1]
+        return None
+    
+    # metodos
+
+    # contar
+    def __len__(self):
+        return len(self.itens)
+
+    # demonstra como esta a pilha
+    def __repr__(self) -> str:
+        if self.esta_vazia():
+            return "Pilha Vazia"
+        
+        # saida da pilha
+        rep = "--- TOPO ---\n"
+        # Imprime na ordem inversa
+        for item in reversed(self.itens):
+            rep += f"  {item}\n"
+        rep += "--- BASE ---"
+        return rep
--- main.py
+++ main.py
@@ -1,2 +1,3 @@
 from Models.produto import Produto
 from Models.engradado import Engradado
+from Estrutura.pilha import Pilha

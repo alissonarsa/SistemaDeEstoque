d292b26f85b358b2c816a6ee1779608cbec1faf7
unknown <alissonarsa@gmail.com>
2025-06-12 13:47:07 -0300

    relatório ok

--- Core/relatorios.py
+++ Core/relatorios.py
@@ -2,10 +2,7 @@ import datetime
 from Core.estoque import Estoque
 from typing import List
 
-# --- Relatório 1: Produtos Próximos ao Vencimento ---
-
 def verificar_vencimento_recursivo(estoque: Estoque, linha: int, coluna: int, produtos_vencendo: List):
-    
     # brek da recursividade
     if linha >= estoque.linhas:
         return
@@ -40,3 +37,52 @@ def gerar_relatorio_vencimento(estoque: Estoque):
         for produto in produtos_vencendo:
             print(f" - Produto: {produto.nome} (Código: {produto.codigo}), Vence em: {produto.data_validade.strftime('%d/%m/%Y')}")
     print("----------------------------------------------------------")
+
+def verificar_falta_recursivo(estoque: Estoque, lista_codigos_produto: List[str], produtos_em_falta: List[str]):
+    if not lista_codigos_produto:
+        return
+        
+    codigo = lista_codigos_produto[0]
+    esta_em_falta = True
+    
+    # tem engradado disponivel para esse produto?
+    for linha, coluna in estoque.mapa_produtos.get(codigo, []):
+        if not estoque.layout[linha][coluna].esta_vazia():
+            esta_em_falta = False
+            break
+            
+    if esta_em_falta:
+        produtos_em_falta.append(codigo)
+        
+    # rec.
+    verificar_falta_recursivo(estoque, lista_codigos_produto[1:], produtos_em_falta)
+
+def gerar_relatorio_itens_em_falta(estoque: Estoque):
+    """Gera o relatório de produtos cadastrados mas sem estoque disponível."""
+    print("\n--- RELATÓRIO: ITENS EM FALTA NO ESTOQUE ---")
+    produtos_em_falta = []
+    codigos_a_verificar = list(estoque.mapa_produtos.keys())
+    verificar_falta_recursivo(estoque, codigos_a_verificar, produtos_em_falta)
+    
+    if not produtos_em_falta:
+        print("Nenhum item em falta.")
+    else:
+        for codigo in produtos_em_falta:
+            print(f" - Produto com código '{codigo}' está sem estoque disponível.")
+    print("---------------------------------------------")
+
+def imprimir_historico_recursivo(lista_pedidos: List):
+    if not lista_pedidos:
+        return
+    
+    print(f" - {lista_pedidos[0]}")
+
+    imprimir_historico_recursivo(lista_pedidos[1:])
+
+def gerar_relatorio_historico(pedidos_atendidos: List):
+    print("\n--- RELATÓRIO: HISTÓRICO DE PEDIDOS ATENDIDOS ---")
+    if not pedidos_atendidos:
+        print("Nenhum pedido foi atendido com sucesso.")
+    else:
+        imprimir_historico_recursivo(pedidos_atendidos)
+    print("--------------------------------------------------")

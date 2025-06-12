9a2a7d8b732ce5d3eccfca4332fa8a7f7b0c8b2e
unknown <alissonarsa@gmail.com>
2025-06-12 09:36:09 -0300

    classe pedido ok

---
+++ Models/pedido.py
@@ -0,0 +1,27 @@
+import datetime
+from typing import NamedTuple # garantir que o pedido seja imutavel
+
+class ItemPedido(NamedTuple):
+    codigo_produto: str
+    quantidade: int
+
+class Pedido:
+    def __init__(self, nome_solicitante: str):
+        self.itens_pedido: list[ItemPedido] = []
+        self.data_solicitacao: datetime.date = datetime.date.today()
+        self.nome_solicitante: str = nome_solicitante
+
+    def adicionar_item(self, codigo_produto: str, quantidade: int):
+        item = ItemPedido(codigo_produto=codigo_produto, quantidade=quantidade)
+        self.itens_pedido.append(item)
+        print(f"Item adicionado ao pedido de {self.nome_solicitante}: {quantidade}x '{codigo_produto}'")
+
+    # metodos
+    
+    def __repr__(self) -> str:
+        # data para o padrão brasileiro
+        data_formatada = self.data_solicitacao.strftime('%d/%m/%Y')
+        # Pega a lista de itens e formata para exibição
+        itens_str = ', '.join([f"{item.quantidade}x '{item.codigo_produto}'" for item in self.itens_pedido])
+        
+        return (f"Pedido(Solicitante: '{self.nome_solicitante}', Data: {data_formatada}, Itens: [{itens_str}])")
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from Models.produto import Produto
 from Models.engradado import Engradado
 from Estrutura.pilha import Pilha
 from Estrutura.fila import Fila
- 
+from Models.pedido import Pedido
